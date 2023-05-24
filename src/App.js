9304a83cee38117ddd2d680f1209c17a16c22cb2
Khiro Sasha <sasha.khiro@gmail.com>
2023-05-24 23:04:42 +0300

    created the initial markup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className='generator'>
+        <h1>ALPACA GENERATOR</h1>
+        <div className='panel'>
+        2
+        </div>
+        <div className='panel'>
+        1
+        </div>
+      </div>
     </div>
   );
 }
