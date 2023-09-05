0bc273238756793eae6fd7e3b802f889936b74ab
Khiro <sasha.khiro@gmail.com>
2023-09-05 22:47:18 +0300

    aded main functional for showed images

--- src/components/alpaca.jsx
+++ src/components/alpaca.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
-import AlpacaArt from './AlpacaArt';
-import { alpacaConfig } from '../utils/alpacaConfig';
-import { getImage } from '../utils/getImage'
+import AlpacaArt from "./AlpacaArt";
+import { alpacaConfig } from "../utils/alpacaConfig";
+import { getImage } from "../utils/getImage";
 
 const Alpaca = () => {
-
-  const [config, setConfig] = useState(alpacaConfig)
+  const [config, setConfig] = useState(alpacaConfig);
   const [bg, setBg] = useState(null);
   const [neck, setNeck] = useState(null);
   const [nose, setNose] = useState(null);
@@ -17,68 +16,67 @@ const Alpaca = () => {
   const [accessories, setAccessories] = useState(null);
 
   const changeImage = (feature, attribute) => {
-    const { directory: dir } = feature
-    const { filename: bgImage } = attribute
+    const { directory: dir } = feature;
+    const { filename: bgImage } = attribute;
 
-    const configClone = [...config]
-    const selectedFeatureIndex = configClone.indexOf(feature)
-    const selectedAttrIndex = configClone[selectedFeatureIndex].items.indexOf(
-      attribute
-    )
+    const configClone = [...config];
+    const selectedFeatureIndex = configClone.indexOf(feature);
+    const selectedAttrIndex =
+      configClone[selectedFeatureIndex].items.indexOf(attribute);
 
     configClone[selectedFeatureIndex].items.forEach(
-      attr => (attr.selected = false)
-    )
-    configClone[selectedFeatureIndex].items[selectedAttrIndex].selected = true
+      (attr) => (attr.selected = false)
+    );
+    configClone[selectedFeatureIndex].items[selectedAttrIndex].selected = true;
 
-    setConfig(configClone)
+    setConfig(configClone);
 
-    getImage(dir, bgImage, image => {
+    getImage(dir, bgImage, (image) => {
       switch (dir) {
-        case 'backgrounds':
-          setBg(image)
-          break
-        case 'neck':
-          setNeck(image)
-          break
-        case 'nose':
-          setNose(image)
-          break
-        case 'eyes':
-          setEyes(image)
-          break
-        case 'ears':
-          setEars(image)
-          break
-        case 'mouth':
-          setMouth(image)
-          break
-        case 'leg':
-          setLeg(image)
-          break
-        case 'hair':
-          setHair(image)
-          break
-        case 'accessories':
-          setAccessories(image)
-          break
+        case "backgrounds":
+          setBg(image);
+          break;
+        case "neck":
+          setNeck(image);
+          break;
+        case "nose":
+          setNose(image);
+          break;
+        case "eyes":
+          setEyes(image);
+          break;
+        case "ears":
+          setEars(image);
+          break;
+        case "mouth":
+          setMouth(image);
+          break;
+        case "leg":
+          setLeg(image);
+          break;
+        case "hair":
+          setHair(image);
+          break;
+        case "accessories":
+          setAccessories(image);
+          break;
         default:
-          break
+          break;
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     const renderAlpaca = () => {
-      config.forEach(feature => {
+      config.forEach((feature) => {
         const attribute = feature.items.filter(
-          at => at.filename === 'default'
-        )[0]
-        changeImage(feature, attribute)
-      })
-    }
-    renderAlpaca()
-  }, [])
+          (at) => at.filename === "default"
+        )[0];
+        changeImage(feature, attribute);
+      });
+    };
+    renderAlpaca();
+  }, []);
 
   const alpacaAttr = {
     bg,
@@ -89,26 +87,17 @@ const Alpaca = () => {
     hair,
     leg,
     ears,
-    accessories
-  }
-
-
-
-
-
-
-
-
+    accessories,
+  };
 
   return (
-    <div className='generator'>
+    <div className="generator">
       <h1>ALPACA GENERATOR</h1>
-      <div className='panel'>
+      
+      <div className="panel">
         <AlpacaArt attr={alpacaAttr} />
       </div>
-      <div className='panel'>
-        1
-      </div>
+      <div className="panel">1</div>
     </div>
   );
 };
