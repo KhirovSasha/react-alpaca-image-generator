a2196d7c39a042c5a7ed4dc85dfd530c47553272
Khiro <sasha.khiro@gmail.com>
2023-09-15 11:55:04 +0300

    added new component of button

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({emoji}) => {
+    return (
+      <div className="bottom-button">
+          {emoji} Button
+      </div>
+    );
+}
+
+export default Button;
